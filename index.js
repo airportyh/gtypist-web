0711a1904e2b392858693694e1ec186afc47536e
airportyh <airportyh@gmail.com>
2013-09-30 00:39:26 -0400

    Made as-you-type higlighting work.

--- index.js
+++ index.js
@@ -6,17 +6,47 @@ function UI(lesson){
   idx(0)
   var exercise = lesson[idx()]
   var expectedText = o()
-  expectedText(exercise.lines.join('\n'))
-  var textarea = h('textarea', {'spellcheck': false}, {
-    onkeydown: onKeyUp
+  var prompt = o()
+  var textarea = h('textarea', {
+    'spellcheck': false, 
+    'rows': 1
+  }, {
+    onkeydown: onKeyDown
   })
-  var complete = o.compute([o.input(textarea), expectedText], function(text, expected){
-    return text === expected
-  })
+  var text = o.input(textarea)
+  var expectedTextDisplay = o.compute(
+    [text, expectedText], function(text, expected){
+      console.log('text', text, 'expected', expected)
+      
+      for (var i = text.length; i--;){
+        var sub = text.substring(0, i + 1)
+        if (expected.substring(0, sub.length) === sub){
+          break
+        }
+      }
+      console.log('i', i, 'sub', sub)
+      if (!expected){
+        return ''
+      }
+      if (!sub){
+        return expected
+      }else{
+        return h('span', 
+            h('span', {className: 'good'}, expected.substring(0, i + 1)),
+            h('span', {className: 'bad'}, expected.substring(i + 1, text.length)),
+            h('span', expected.substring(text.length))
+          )
+      }
+    })
+  var complete = o.compute(
+    [text, expectedText], function(text, expected){
+      return text === expected
+    })
   var preClass = o.compute([complete], function(good){
     return good ? 'good': ''
   })
-  function onKeyUp(e){
+  advanceExercise(exercise)
+  function onKeyDown(e){
     if (e.keyCode === 13){
       e.preventDefault()
       if (complete()){
@@ -27,15 +57,23 @@ function UI(lesson){
   function nextExercise(){
     idx(idx() + 1)
     exercise = lesson[idx()]
+    advanceExercise(exercise)
+    
+  }
+  function advanceExercise(exercise){
+    text('')
     expectedText(exercise.lines.join('\n'))
-    textarea.value = ''
+    prompt(exercise.prompt)
   }
   var ui = h('div',
-    h('label', exercise.prompt),
-    h('pre', {'className': preClass}, expectedText),
-    textarea,
-    h('label', 'idx:', idx)
+    h('label', prompt),
+    h('pre', {'className': preClass}, expectedTextDisplay),
+    textarea
   )
+
+  setTimeout(function(){
+    textarea.focus()
+  }, 0)
   return ui
 }
 
